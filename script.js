8559e23faff76e702909656a380d4f379347a2ec
nrbrt2002 <nrbrt2002@gmail.com>
2023-08-04 22:55:54 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let weather = {
+    apiKey: "cb1c7b48978957bb44b78464ea6f1882",
+    fetchWeather: function (city){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=" + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data)=> this.displayWeather(data));
+    },
+    displayWeather: function(data){
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const{temp, humidity} = data.main;
+        const{speed} = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector('.city').innerHTML = "Weather in " +name;
+        document.querySelector('.temp').innerHTML = temp + "°";
+        document.querySelector('.icon').src = "https://openweathermap.org/img/wn/"+ icon +".png";
+        document.querySelector('.description').innerHTML = description;
+        document.querySelector('.hummidity').innerHTML = "Hummidity:  " + humidity;
+        document.querySelector('.wind').innerHTML = "Wind Speed:  " + speed + "Km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+ name +"')"
+
+    },
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+}   
+document.querySelector(".search button").addEventListener("click", function(){
+    weather.search();
+});
+document.querySelector(".search-bar").addEventListener('keyup', function(event){
+    if(event.key == "Enter"){
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Kigali");
